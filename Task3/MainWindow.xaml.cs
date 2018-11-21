f9b85f507369fe567142de96334737ea552b859d
ihorHuralskyy <uniqueihor@gmail.com>
2018-11-22 00:46:04 +0200

    added all buttons, new datagrid for order and some bugs were fixed

--- Task3/MainWindow.xaml.cs
+++ Task3/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using OrderLib;
+using System.Data;
 using System.IO;
 using Task3;
 
@@ -27,28 +28,28 @@ namespace Task3
         public MainWindow()
         {
             InitializeComponent();
-            //Order newO = new Order("q");
-            //newO.addToOrder(new Nigiri());
-            //newO.addToOrder(new Maki());
-            //newO.submitOrder();
+            ////Order newO = new Order("q");
+            ////newO.addToOrder(new Nigiri());
+            ////newO.addToOrder(new Maki());
+            ////newO.submitOrder();
 
-            //Order newOo = new Order("w");
-            //newOo.addToOrder(new Sashimi());
-            //newOo.addToOrder(new Uramaki());
-            //newOo.submitOrder();
+            ////Order newOo = new Order("w");
+            ////newOo.addToOrder(new Sashimi());
+            ////newOo.addToOrder(new Uramaki());
+            ////newOo.submitOrder();
 
-            //Order kk = new Order();
-            //StreamReader streamReader = new StreamReader(@"C:\Task3\SBTDDTask3\OrderLib\Orders.txt");
-            //var t = kk.findOrderByName("q", streamReader);
-            //streamReader.Close();
-            //t.submitOrder();
+            ////Order kk = new Order();
+            ////StreamReader streamReader = new StreamReader(@"C:\Task3\SBTDDTask3\OrderLib\Orders.txt");
+            ////var t = kk.findOrderByName("q", streamReader);
+            ////streamReader.Close();
+            ////t.submitOrder();
 
-            ////inserting data griedview with all catalog of availbaile sushis
-            //List<Sushi> allSushis = new List<Sushi> { new Maki(), new Nigiri(), new Sashimi(), new Uramaki(), new Rolls(), new Cheaps() };
-            //foreach (Sushi s in allSushis)
-            //{
-            //    SushiDataGrid.Items.Add(s);
-            //}
+            //////inserting data griedview with all catalog of availbaile sushis
+            ////List<Sushi> allSushis = new List<Sushi> { new Maki(), new Nigiri(), new Sashimi(), new Uramaki(), new Rolls(), new Cheaps() };
+            ////foreach (Sushi s in allSushis)
+            ////{
+            ////    SushiDataGrid.Items.Add(s);
+            ////}
 
         }
 
@@ -73,16 +74,103 @@ namespace Task3
 
         }
 
-        //realization of adding to order list selected item
+        ////realization of adding to order list selected item
         private void AddToOrderButton_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                String ProductName = (SushiDataGrid.SelectedItem as Sushi).Name; ;
+                switch (ProductName)
+                {
+                    case "Nigiri":
+                        order.addToOrder(new Nigiri());
+                        SushiOrder.Items.Add(order.sushis[order.sushis.Count - 1]);
+                        textBox.Text = order.TotalPrice.ToString();
+                        break;
+                    case "Maki":
+                        order.addToOrder(new Maki());
+                        SushiOrder.Items.Add(order.sushis[order.sushis.Count - 1]);
+                        textBox.Text = order.TotalPrice.ToString();
+                        break;
+                    case "Uramaki":
+                        order.addToOrder(new Uramaki());
+                        SushiOrder.Items.Add(order.sushis[order.sushis.Count - 1]);
+                        textBox.Text = order.TotalPrice.ToString();
+                        break;
+                    case "Sashimi":
+                        order.addToOrder(new Sashimi());
+                        SushiOrder.Items.Add(order.sushis[order.sushis.Count - 1]);
+                        textBox.Text = order.TotalPrice.ToString();
+                        break;
+                    case "Cheaps":
+                        order.addToOrder(new Cheaps());
+                        SushiOrder.Items.Add(order.sushis[order.sushis.Count - 1]);
+                        textBox.Text = order.TotalPrice.ToString();
+                        break;
+                    case "Rolls":
+                        order.addToOrder(new Rolls());
+                        SushiOrder.Items.Add(order.sushis[order.sushis.Count - 1]);
+                        textBox.Text = order.TotalPrice.ToString();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
         }
 
-        //realization of commiting order (save to txt file)
+        private void DeleteFromOrderButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                String ProductName = (SushiOrder.SelectedItem as Sushi).Name; ;
+                switch (ProductName)
+                {
+                    case "Nigiri":
+                        order.deleteFromOrder(new Nigiri());
+                        SushiOrder.Items.Remove((SushiOrder.SelectedItem as Sushi));
+                        textBox.Text = order.TotalPrice.ToString();
+                        break;
+                    case "Maki":
+                        order.deleteFromOrder(new Maki());
+                        SushiOrder.Items.Remove((SushiOrder.SelectedItem as Sushi));
+                        textBox.Text = order.TotalPrice.ToString();
+                        break;
+                    case "Uramaki":
+                        order.deleteFromOrder(new Uramaki());
+                        SushiOrder.Items.Remove((SushiOrder.SelectedItem as Sushi));
+                        textBox.Text = order.TotalPrice.ToString();
+                        break;
+                    case "Sashimi":
+                        order.deleteFromOrder(new Sashimi());
+                        SushiOrder.Items.Remove((SushiOrder.SelectedItem as Sushi));
+                        textBox.Text = order.TotalPrice.ToString();
+                        break;
+                    case "Cheaps":
+                        order.deleteFromOrder(new Cheaps());
+                        SushiOrder.Items.Remove((SushiOrder.SelectedItem as Sushi));
+                        textBox.Text = order.TotalPrice.ToString();
+                        break;
+                    case "Rolls":
+                        order.deleteFromOrder(new Rolls());
+                        SushiOrder.Items.Remove((SushiOrder.SelectedItem as Sushi));
+                        textBox.Text = order.TotalPrice.ToString();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    
+        ////realization of commiting order (save to txt file)
         private void commitOrderClick(object sender, RoutedEventArgs e)
         {
-
+            order.submitOrder();
+            textBox.Text = 0.ToString();
+            SushiOrder.Items.Clear();
         }
 
         private void btnLogOut_Click(object sender, RoutedEventArgs e)
@@ -115,5 +203,7 @@ namespace Task3
 
             }
         }
+
+       
     }
 }
